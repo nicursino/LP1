0dadc2c0a2d9edd9e4684dbd8d3c794afd07b355
nicursino <nicolascursino@gmail.com>
2024-03-13 20:22:40 -0300

    add TesteClasse

---
+++ Tarefa3/classes/src/TesteClasse.java
@@ -0,0 +1,34 @@
+public class TesteClasse {
+    public static void  main(String[] args){
+        //CorpoHumano
+        String sexo;
+
+        //televisao
+        String assistir;
+
+        //celular
+        String fotografar;
+
+        //aviao
+        String pilotar;
+
+        //quadras
+        String brincar;
+
+        //roupas
+        String experimentar;
+
+        //Condominio
+        String morar;
+
+        //Carros
+        String acelerar;
+
+        //Lojas
+        String comprar;
+
+        //Flores
+        String petalas;
+
+    }
+}
